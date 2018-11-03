a28f89136da457a8fdd8494a95705be8ae61de9b
Tom Austin <tom.austin@streets-heaver.com>
2018-11-03 14:59:16 +0000

    Init

---
+++ Typa/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Typa.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typa.ViewModels
+{
+    public abstract class BaseViewModel : BaseNotify
+    {
+       
+        public BaseViewModel()
+        {
+            Loaded();
+        }
+
+        public abstract void Loaded();
+    }
+}
---
+++ Typa/ViewModels/TypaViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using Typa.Classes;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Typa.ViewModels
+{
+    public class TypaViewModel : BaseViewModel
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+        public TypaViewModel()
+        {
+            SampleCommand = new DelegateCommand(SampleMethod, CanSampleMethod);
+
+
+        }
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand SampleCommand { get; set; }
+
+
+
+        #endregion
+
+        #region Methods
+
+        public override void Loaded()
+        {
+
+        }
+
+        void SampleMethod()
+        {
+
+        }
+
+        bool CanSampleMethod()
+        {
+            return true;
+        }
+
+       
+
+        #endregion
+    }
+}
