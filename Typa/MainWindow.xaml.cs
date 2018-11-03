e5cae5f5d74e11eb10e9d6bc751ccc4a213bbcc9
Tom Austin <tom.austin@streets-heaver.com>
2018-11-03 16:38:35 +0000

    Added basic functionality, need to sort out trayicon closing and other issues

--- Typa/MainWindow.xaml.cs
+++ Typa/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using Typa.Helpers;
+using WindowsInput;
 
 namespace Typa
 {
@@ -20,9 +11,72 @@ namespace Typa
     /// </summary>
     public partial class MainWindow : Window
     {
+        private uint _hotKey1;
+        private HotKeyHelper _hotKeys;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _hotKeys = new HotKeyHelper(this, HandleHotKey);
+            _hotKey1 = _hotKeys.ListenForHotKey(System.Windows.Forms.Keys.V, HotKeyModifiers.Control | HotKeyModifiers.Alt);
+
+            notifyIcon.Visibility = Visibility.Collapsed;
+            notifyIcon.Visibility = Visibility.Visible;
+
+            //Hide();
+
+
         }
+
+
+        // minimize to system tray when applicaiton is minimized
+        //protected override void OnStateChanged(EventArgs e)
+        //{
+        //    if (WindowState == WindowState.Minimized)
+        //    {
+        //        Hide();
+        //    }
+
+
+        //    base.OnStateChanged(e);
+        //}
+
+        // minimize to system tray when applicaiton is closed
+        //protected override void OnClosing(CancelEventArgs e)
+        //{
+        //    // setting cancel to true will cancel the close request
+        //    // so the application is not closed
+
+        //    e.Cancel = true;
+
+        //    Hide();
+
+        //    base.OnClosing(e);
+        //}
+
+        //protected override void OnSourceInitialized(EventArgs e)
+        //{
+        //    base.OnSourceInitialized(e);
+
+
+        //}
+
+
+
+        [STAThread]
+        void HandleHotKey(int keyId)
+        {
+            if (keyId == _hotKey1)
+            {
+                var clipboardText = System.Windows.Clipboard.GetText();
+                
+                var test = new InputSimulator();
+                test.Keyboard.TextEntry(clipboardText);
+            }
+
+        }
+
+      
     }
 }
